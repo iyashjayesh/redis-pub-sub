998eeaf3eb351859fef476b434c627e358a432d1
Yash Chauhan <ychauhan@altair.com>
2022-11-24 13:41:10 +0530

    log added

--- publish.go
+++ publish.go
@@ -10,6 +10,7 @@ func main() {
 
 	//Publishing events
 	client := utils.CreateClient()
+	log.Println("Publishing to the monitor channel")
 	for i := 0; i < 100; i++ {
 		utils.PublishMessage(client, "monitor", "This is Message from Monitor")
 		log.Println("Published message from Monitor Subject")
